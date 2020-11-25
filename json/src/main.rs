550e7934c4f805fdeeb151ae2b53e085a1d4ab88
XFFXFF <1247714429@qq.com>
2020-11-25 21:13:28 +0800

    Serialize and deserialize a data structure with serde json

---
+++ json/src/main.rs
@@ -0,0 +1,41 @@
+use serde::{Deserialize, Serialize};
+use std::io::prelude::*;
+use std::io::BufWriter;
+use std::io::BufReader;
+use std::fs::File;
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Direction {
+    East,
+    South,
+    West,
+    North,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Move {
+    direction: Direction,
+    step: u32,
+}
+
+fn main() {
+    let m = Move {
+        direction: Direction::East,
+        step: 5
+    };
+
+    let serialized = serde_json::to_string(&m).unwrap();
+    let f = File::create("move.txt").unwrap();
+    {
+        let mut writer = BufWriter::new(f);
+        writer.write(serialized.as_bytes()).unwrap();
+    }
+
+    let f = File::open("move.txt").unwrap();
+    let mut reader = BufReader::new(f);
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer).unwrap();
+
+    let deserialized: Move = serde_json::from_str(&buffer).unwrap();
+    println!("{:?}", deserialized);
+}
