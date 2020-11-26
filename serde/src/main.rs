0c387cd07588177fdb1291b804b4a6a9f77c8bd3
XFFXFF <1247714429@qq.com>
2020-11-26 20:29:45 +0800

    serde_json to_writer and from_reader

--- serde/src/main.rs
+++ serde/src/main.rs
@@ -1,8 +1,6 @@
 use serde::{Deserialize, Serialize};
 use ron::ser::{to_string_pretty, PrettyConfig};
 use ron::de::from_str;
-use std::io::prelude::*;
-use std::io::BufWriter;
 use std::io::BufReader;
 use std::fs::File;
 
@@ -21,19 +19,13 @@ struct Move {
 }
 
 fn json_example(m: &Move) {
-    let serialized = serde_json::to_string(&m).unwrap();
     let f = File::create("move.txt").unwrap();
-    {
-        let mut writer = BufWriter::new(f);
-        writer.write(serialized.as_bytes()).unwrap();
-    }
+    serde_json::to_writer(f, m).unwrap();
 
     let f = File::open("move.txt").unwrap();
-    let mut reader = BufReader::new(f);
-    let mut buffer = String::new();
-    reader.read_line(&mut buffer).unwrap();
+    let reader = BufReader::new(f);
+    let deserialized: Move = serde_json::from_reader(reader).unwrap();
 
-    let deserialized: Move = serde_json::from_str(&buffer).unwrap();
     println!("{:?}", deserialized);
 }
 
